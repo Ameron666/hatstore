2e03b2c148a3cdae22b8e7e64a0daa2f74232c76
Ameron666 <aisakebi69@gmail.com>
2023-12-12 08:59:32 +0300

    big update

--- js/main.js
+++ js/main.js
@@ -48,3 +48,36 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+
+// JavaScript для обработки событий нажатия на кнопку
+
+// Получаем все элементы с классом "dropdown"
+var dropdowns = document.querySelectorAll('.dropdown');
+var otherElements = document.querySelector('.other-elements');
+
+// Добавляем обработчик события для каждого элемента
+dropdowns.forEach(function(dropdown) {
+  var button = dropdown.querySelector('.dropbtn');
+  var content = dropdown.querySelector('.dropdown-content');
+
+  // Обработчик события для нажатия на кнопку
+  button.addEventListener('click', function() {
+    // Переключаем видимость контейнера со ссылками
+    content.classList.toggle('show');
+
+    // Устанавливаем margin-top для other-elements в зависимости от высоты dropdown-content
+    if (content.classList.contains('show')) {
+      otherElements.style.marginTop = content.clientHeight + 'px';
+    } else {
+      otherElements.style.marginTop = '0';
+    }
+  });
+
+  // Закрываем контейнер со ссылками, если клик произошел вне элемента
+  window.addEventListener('click', function(event) {
+    if (!event.target.matches('.dropbtn') && !event.target.closest('.dropdown')) {
+      content.classList.remove('show');
+      otherElements.style.marginTop = '0';
+    }
+  });
+});
